6ccaaf6c6a7a429328c47bf0d6ac3194de6572ec
Osblouf <osblouf@gmail.com>
2013-10-16 21:11:13 +0200

    Update main.c
    Les options rs232 sont a vérifier

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <16F877A.h>
+
 #device adc=8
+
 #FUSES NOWDT      //No Watch Dog Timer
 #FUSES HS         //Highspeed Osc > 4mhz
 #FUSES PUT        //Power Up Timer
@@ -13,18 +15,33 @@
 #use delay(clock=8000000) //fréquence
 #use rs232(baud=57600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
 
+//definition de la pin output
+#define PIN_OUTPUT_LED PIN_C2
+
 //fonction d'envoi RC5
 void RC5(char c);
+void LED_1(void);
+void LED_0(void);
 
 void main(void)
 {
+  //ressources
   char c;
+  
+  //pin output à 1 (led éteinte)
+  output_high(PIN_OUTPUT_LED);
   
   //boucle
   while(1)
   {
+    //récupération de l'octet
     c = getc();
+    
+    //envoi à la LED
     RC5(c);
+    
+    //repassage en mode repos
+    output_high(PIN_OUTPUT_LED);
   }
 }
 
@@ -32,5 +49,73 @@ void main(void)
 //fonction d'envoi RC5
 void RC5(char c)
 {
+  //deux premiers bits
+  LED_1();
+  LED_1();
   
+  //bits à 0
+  LED_0();
+  LED_0();
+  LED_0();
+  LED_0();
+  
+  //envoi du message :
+  if(c & (1 << 7))
+    LED_1();
+  else
+    LED_0();
+    
+  if(c & (1 << 6))
+    LED_1();
+  else
+    LED_0();
+    
+  if(c & (1 << 5))
+    LED_1();
+  else
+    LED_0();
+  
+  if(c & (1 << 4))
+    LED_1();
+  else
+    LED_0();
+  
+  if(c & (1 << 3))
+    LED_1();
+  else
+    LED_0();
+  
+  if(c & (1 << 2))
+    LED_1();
+  else
+    LED_0();
+  
+  if(c & (1 << 1))
+    LED_1();
+  else
+    LED_0();
+    
+  if(c & (1 << 0))
+    LED_1();
+  else
+    LED_0();
+  
+}
+
+//fonction bit à 1
+void LED_1(void)
+{
+  output_high(PIN_OUTPUT_LED);
+  delay_us(889);
+  output_low(PIN_OUTPUT_LED);
+  delay_us(889);
+}
+
+//fonction bit à 0
+void LED_0(void)
+{
+  output_low(PIN_OUTPUT_LED);
+  delay_us(889);
+  output_high(PIN_OUTPUT_LED);
+  delay_us(889);
 }

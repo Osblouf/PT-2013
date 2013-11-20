29d689153df65ecaa55672f5662649ae5441af19
Alexandre Brand <brand@ece.fr>
2013-11-20 16:38:55 +0100

    IRTransmitter Final Working Interface
    All functional !!!
    (Added : Read/Write into ports buffer).
    (Not implemented yet : setparity, setstopbits)

--- Interface-PC/IRtransmitter/IRtransmitter/Form1.cs
+++ Interface-PC/IRtransmitter/IRtransmitter/Form1.cs
@@ -28,11 +28,14 @@ namespace IRtransmitter
 
         public Form1()
         {
-            // Loading User Default Settings...
-            //InitializeDefaultUserSettings();
-
+            
             // Initialize Window
             InitializeComponent();
+
+            // Debug prompt
+            this.rtfTerminal.Text += "Components Initialized\n";
+            this.rtfTerminal.Text += "Initial State of Port :\n";
+            this.port_Show_Status();
 
             // Setting Events on Serial Port
             comport.DataReceived += new SerialDataReceivedEventHandler(port_DataIn);
@@ -64,29 +67,6 @@ namespace IRtransmitter
                 else
                     this.rtfTerminal.Text += "\n \n COMPORT Not Opened";
             }
-        }
-
-        private void port_Show_Status()
-        {
-            try
-            {
-                this.rtfTerminal.Text += "COMPORT : " + this.comport.ToString();
-                this.rtfTerminal.Text += "\n";
-            }
-            catch (Exception e)
-            {
-                this.rtfTerminal.Text += e.ToString();
-            }
-        }
-
-        private void InitializeDefaultUserSettings()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void comport_PinChanged(object sender, SerialPinChangedEventArgs e)
-        {
-            throw new NotImplementedException();
         }
 
         private void senddata_Click(object sender, EventArgs e)
@@ -126,103 +106,92 @@ namespace IRtransmitter
                 case "status":
                     this.prompt_status(words);
                     break;
+                case "setparity":
+                    print("Not implemented yet.");
+                    break;
+                case "setstopbits":
+                    print("Not implemented yet.");
+                    break;
+                case "closeport":
+                    print("Closing Port...");
+                    this.prompt_closeport();
+                    break;
+                case "readport":
+                    print("Reading Data...");
+                    this.prompt_readport();
+                    break;
+                default :
+                    print("Unknown command. Type help for more information.");
+                    break;
             }
 
         }
 
+        #region PROMPT COMMANDS DETAIL
         
-        #region PORT FUNCTIONS SECTION
-        private void port_SendData(string dataToBeSent)
+        /// <summary>
+        /// Reads all existing value on the port buffer
+        /// </summary>
+        private void prompt_readport()
+        {
+            if (this.comport.IsOpen)
+            {
+                this.port_DataIn(null, null);
+            }
+            else print("No Port Open.");
+        }
+
+        /// <summary>
+        /// Close Serial port. 
+        /// </summary>
+        private void prompt_closeport()
         {
             try
             {
-                // Convert the user's string of hex digits (ex: B4 CA E2) to a byte array
-                byte[] data = HexStringToByteArray(dataToBeSent);
-
-                // Send the binary data out the port
-                comport.Write(data, 0, data.Length);
-
-                // Show the hex digits on in the terminal window
-                this.rtfTerminal.Text += "Prompt : " + ByteArrayToHexString(data) + "\n";
+                this.comport.Close();
             }
-            catch (FormatException)
+            catch (Exception e) { print("Error : " + e + "\n"); }
+            finally
             {
-                // Inform the user if the hex string was not properly formatted
-                this.rtfTerminal.Text += "ALERT : Not properly formatted hex string: " + dataToBeSent + "\n";
-            }
-        }
-
-        private string port_ReadData()
-        {
-            String ret = "no new values in buffer \n";
-            // If the com port has been closed, do nothing
-			if (!comport.IsOpen) return ret;
-
-            // This method will be called when there is data waiting in the port's buffer
-            else{
-                // Obtain the number of bytes waiting in the port's buffer
-                int bytes = comport.BytesToRead;
-
-                // Create a byte array buffer to hold the incoming data
-                byte[] buffer = new byte[bytes];
-
-                // Read the data from the port and store it in our buffer
-                comport.Read(buffer, 0, bytes);
-
-                // Change buffer ToString
-                try{
-                ret = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
-                }
-                catch(Exception e){ this.rtfTerminal.Text += e; }
-                return ret;
+                if (!comport.IsOpen) print("Port closed properly. \n");
+                else print("Program did not manage to close port. \n");
             }
         }
 
         /// <summary>
-        /// Lists Ports Name
+        /// Show Current Status of the Serial Port. 
         /// </summary>
-        /// <returns></returns>
-        private string[] OrderedPortNames()
+        private void port_Show_Status()
         {
-            // Just a placeholder for a successful parsing of a string to an integer
-            int num;
-
-            // Order the serial port names in numberic order (if possible)
-            return SerialPort.GetPortNames().OrderBy(a => a.Length > 3 && int.TryParse(a.Substring(3), out num) ? num : 0).ToArray();
+            try
+            {
+                this.rtfTerminal.Text += "COMPORT : " + this.comport.ToString();
+                this.rtfTerminal.Text += "\n Baud | DataBits | Parity | StopBits | IsOpen \n";
+                this.rtfTerminal.Text += "\n " + this.comport.BaudRate + " | " + this.comport.DataBits + " | " + this.comport.Parity + " | " + this.comport.StopBits + " | " + this.comport.IsOpen + " | ";
+            }
+            catch (Exception e)
+            {
+                this.rtfTerminal.Text += e.ToString();
+            }
         }
-
-        /// <summary> Convert a string of hex digits (ex: E4 CA B2) to a byte array. </summary>
-        /// <param name="s"> The string containing the hex digits (with or without spaces). </param>
-        /// <returns> Returns an array of bytes. </returns>
-        private byte[] HexStringToByteArray(string s)
-        {
-            s = s.Replace(" ", "");
-            byte[] buffer = new byte[s.Length / 2];
-            for (int i = 0; i < s.Length; i += 2)
-                buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
-            return buffer;
-        }
-
-        /// <summary> Converts an array of bytes into a formatted string of hex digits (ex: E4 CA B2)</summary>
-        /// <param name="data"> The array of bytes to be translated into a string of hex digits. </param>
-        /// <returns> Returns a well formatted string of hex digits with spacing. </returns>
-        private string ByteArrayToHexString(byte[] data)
-        {
-            StringBuilder sb = new StringBuilder(data.Length * 3);
-            foreach (byte b in data)
-                sb.Append(Convert.ToString(b, 16).PadLeft(2, '0').PadRight(3, ' '));
-            return sb.ToString().ToUpper();
-        }
-        #endregion
-
-        #region PROMPT COMMANDS DETAIL
 
         /// <summary>
         /// prints a summary of commands
         /// </summary>
         private void printHelp()
         {
-            String commandList = "Commands Available :\n send [DATA - String] \n setbaud [RATE Int ] \n connect [Void] \n echo [DATA String] \n clear [Void] \n listPortsName [void] \n status [void] \n";
+            String commandList = "send \n" 
+                                 + "setbaud \n"
+                                 + "connect \n"
+                                 + "echo \n"
+                                 + "clear \n"
+                                 + "help \n"
+                                 + "listPortsName \n"
+                                 + "status \n"
+                                 + "setparity \n"
+                                 + "setstopbits \n"
+                                 + "closeport \n"
+                                 + "readport \n";
             this.rtfTerminal.Text += commandList;
         }
 
@@ -329,20 +298,118 @@ namespace IRtransmitter
             this.port_Show_Status();
         }
         #endregion
-
         
+        #region PORT FUNCTIONS SECTION
 
+        void comport_PinChanged(object sender, SerialPinChangedEventArgs e)
+        {
+            // Show the state of the pins
+            UpdatePinState();
+        }
+
+        private void UpdatePinState()
+        {
+            this.Invoke(new ThreadStart(() =>
+            {
+                // Show the state of the pins
+                // chkCD.Checked = comport.CDHolding;
+                // chkCTS.Checked = comport.CtsHolding;
+                // chkDSR.Checked = comport.DsrHolding;
+            }));
+        }
+
+        private void port_DataIn(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (this.comport.IsOpen) print("Port : " + this.comport.ReadExisting());
+            else return;
+        }
+
+        private void port_SendData(string dataToBeSent)
+        {
+            try
+            {
+                // Convert the user's string of hex digits (ex: B4 CA E2) to a byte array
+                byte[] data = HexStringToByteArray(dataToBeSent);
+
+                // Send the binary data out the port
+                comport.Write(data, 0, data.Length);
+
+                // Show the hex digits on in the terminal window
+                this.rtfTerminal.Text += "Prompt : " + ByteArrayToHexString(data) + "\n";
+            }
+            catch (FormatException)
+            {
+                // Inform the user if the hex string was not properly formatted
+                this.rtfTerminal.Text += "ALERT : Not properly formatted hex string: " + dataToBeSent + "\n";
+            }
+        }
+
+        private string port_ReadData()
+        {
+            String ret = "no new values in buffer \n";
+            // If the com port has been closed, do nothing
+			if (!comport.IsOpen) return ret;
+
+            // This method will be called when there is data waiting in the port's buffer
+            else{
+                // Obtain the number of bytes waiting in the port's buffer
+                int bytes = comport.BytesToRead;
+
+                // Create a byte array buffer to hold the incoming data
+                byte[] buffer = new byte[bytes];
+
+                // Read the data from the port and store it in our buffer
+                comport.Read(buffer, 0, bytes);
+
+                // Change buffer ToString
+                try{
+                ret = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+                }
+                catch(Exception e){ this.rtfTerminal.Text += e; }
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Lists Ports Name
+        /// </summary>
+        /// <returns></returns>
+        private string[] OrderedPortNames()
+        {
+            // Just a placeholder for a successful parsing of a string to an integer
+            int num;
+
+            // Order the serial port names in numberic order (if possible)
+            return SerialPort.GetPortNames().OrderBy(a => a.Length > 3 && int.TryParse(a.Substring(3), out num) ? num : 0).ToArray();
+        }
+
+        /// <summary> Convert a string of hex digits (ex: E4 CA B2) to a byte array. </summary>
+        /// <param name="s"> The string containing the hex digits (with or without spaces). </param>
+        /// <returns> Returns an array of bytes. </returns>
+        private byte[] HexStringToByteArray(string s)
+        {
+            s = s.Replace(" ", "");
+            byte[] buffer = new byte[s.Length / 2];
+            for (int i = 0; i < s.Length; i += 2)
+                buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
+            return buffer;
+        }
+
+        /// <summary> Converts an array of bytes into a formatted string of hex digits (ex: E4 CA B2)</summary>
+        /// <param name="data"> The array of bytes to be translated into a string of hex digits. </param>
+        /// <returns> Returns a well formatted string of hex digits with spacing. </returns>
+        private string ByteArrayToHexString(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder(data.Length * 3);
+            foreach (byte b in data)
+                sb.Append(Convert.ToString(b, 16).PadLeft(2, '0').PadRight(3, ' '));
+            return sb.ToString().ToUpper();
+        }
+        #endregion
 
         private void label2_Click(object sender, EventArgs e)
         {
             
-        }
-
-
-
-        private void port_DataIn(object sender, SerialDataReceivedEventArgs e)
-        {
-            throw new NotImplementedException();
         }
     }
 }
